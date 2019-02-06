e85815d5a2cdd254db9653a1244f43bd16cf6676
thalliscs <thalliscs@gmail.com>
2019-02-06 18:49:32 -0200

    machine controller

---
+++ ApiCurso/Controllers/MachineController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/machine")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        public IActionResult GetMachineName()
+        {
+            return Ok(new { name = Environment.MachineName });
+        }
+    }
+}
