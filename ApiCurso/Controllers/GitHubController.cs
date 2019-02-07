eb8920057cadde66c0a3c48bef391b8f5e80de90
thalliscs <thalliscs@gmail.com>
2019-02-07 13:21:35 -0200

    teste comunicação externa para demonstrar uso do recurso Application map

---
+++ ApiCurso/Controllers/GitHubController.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCurso.Controllers
+{
+    [Route("api/github")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        public async Task<IActionResult> GetDotNetInfo()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("applicationException/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var data = await client.GetStringAsync("https://api.github.com/orgs/donet/repos");
+            return Ok(data);
+        }
+    }
+}
