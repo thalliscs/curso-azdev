13030ad7a164f819c4a415f4a3a17d641a194a49
thalliscs <thalliscs@gmail.com>
2019-02-07 13:11:02 -0200

    criação web jobs

---
+++ WebJobsTarefas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WebJobsTarefas
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enviando e-mail para novo usuário!");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        }
+    }
+}
